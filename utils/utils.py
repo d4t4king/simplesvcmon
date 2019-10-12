4f663348ffaa6fa5e9381bbbde344c0955030828
d4t4king <dataking@gmail.com>
2019-10-11 22:00:18 -0700

    is_alive()

--- utils/utils.py
+++ utils/utils.py
@@ -12,3 +12,14 @@ class utils():
             p = os.path.join(p, pgm)
             if os.path.exists(p) and os.access(p, os.X_OK):
                 return p
+
+
+    @staticmethod
+    def is_alive(host, port=None):
+        from scapy.all import sr, srp, IP, UDP, ICMP, TCP, ARP, Ether
+        ans, unans = sr(IP(dst=host)/ICMP(), retry=0, timeout=1)
+        print("ans: {a}, unans: {u}".format(a=ans, u=unans))
+        if ans is not None:
+            return True
+        else:
+            return False
